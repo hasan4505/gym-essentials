2d8f3cda67eb8e6b6229b13c77e2c75e2e47e896
hassan <mijuahmed5@gmail.com>
2022-04-18 17:19:55 +0600

    404 page updated

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -1,14 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./NotFound.css";
 
 const NotFound = () => {
   return (
-    <div>
-      <h1>404</h1>
-      <p>The Page You Were Looking For Couldn't Be Found</p>
-      <Link to="/home" variant="primary" type="submit">
-        Go to homepage
-      </Link>
+    <div className="not-found">
+      <h1>Oops!</h1>
+      <h3>404 - Page Not Found</h3>
+      <p>
+        The page you're looking for might have been removed, had it's name
+        changed or is temporarily unavailable.
+      </p>
+      <div>
+        <Link to="/home" className="not-found-btn">
+          Go to homepage
+        </Link>
+      </div>
     </div>
   );
 };
